b1fde1fd686a8ea7058bf7a93355c44470f2dd0b
RealSilo <szilard.magyar@gmail.com>
2016-08-19 00:52:38 +0200

    app

---
+++ app/assets/javascripts/chatrooms.js
@@ -0,0 +1,19 @@
+$(document).on("turbolinks:load", function () {
+
+
+});
+
+$(document).on("keypress", "#new_message", function (e) {
+  if (e && e.keyCode === 13 && e.target.value.length > 0) {
+    e.preventDefault();
+    $(this).submit();
+  }
+});
+
+$(document).on("submit", "#new_message", function (e) {
+  e.preventDefault();
+  var chatroom_id = $("[data-behavior='messages']").data("chatroom-id");
+  var body = $("#message_body");
+  App.chatrooms.send_message(chatroom_id, body.val());
+  body.val("");
+});
