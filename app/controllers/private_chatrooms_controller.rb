64a70c2411560b23f2c568d4b0bcc3b8e39af714
RealSilo <szilard.magyar@gmail.com>
2016-09-28 13:30:48 +0200

    removing debugging code

--- app/controllers/private_chatrooms_controller.rb
+++ app/controllers/private_chatrooms_controller.rb
@@ -7,9 +7,6 @@ class PrivateChatroomsController < ApplicationController
       redirect_to :back
     else
       @chatroom = Chatroom.private_chatroom_for_users(users)
-      puts "HAHAHAHAHAHAHA"
-      puts @chatroom.id
-      puts "HAHAHAHAHAHAHAH"
       @messages = @chatroom.messages.latest
       @chatroom_user = current_user.chatroom_users.find_by(chatroom_id: @chatroom.id)
       render "chatrooms/show"
