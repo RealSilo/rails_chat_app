c9ebea9d7d9c29b6e52931b1815d90b1e4d70711
RealSilo <szilard.magyar@gmail.com>
2016-10-05 21:47:23 +0200

    modifying channel test

--- spec/features/message_broadcasting_spec.rb
+++ spec/features/message_broadcasting_spec.rb
@@ -6,13 +6,13 @@ feature "broadcasting message" do
   let(:posting_user) { create(:user) }
   let(:chatroom) { create(:chatroom, owner: user, name: "group chatroom", private_chatroom: false) }
   let!(:chatroom_user) { create(:chatroom_user, chatroom: chatroom, user: user) }
-  let!(:post_chatroom_user) { create(:chatroom_user, chatroom: chatroom, user: posting_user) }
+  let!(:posting_chatroom_user) { create(:chatroom_user, chatroom: chatroom, user: posting_user) }
 
   scenario "successfully", :js => true do
     sign_in(user)
     visit chatroom_path(chatroom)
     expect(page).not_to have_content("this is a message")
-    message = Message.create(body: "this is a message", user: other_user, chatroom: chatroom)
+    message = Message.create(body: "this is a message", user: posting_user, chatroom: chatroom)
     MessageRelayJob.perform_now(message)
     #fill_in "message[body]", with: "this is a message"
     #find('#new_message').native.send_keys(:return)
